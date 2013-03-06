55ac0ffd66a22cfbf6bd49292516ea01647e689b
181002299 <181002299@Sala603-04.fspoaeduc.com.br>
2013-03-05 22:05:15 -0300

    Versão 1.0

--- entregas/trabalho2/src/main/java/br/com/senacrs/alp/aulas/Somatorio.java
+++ entregas/trabalho2/src/main/java/br/com/senacrs/alp/aulas/Somatorio.java
@@ -1,27 +1,35 @@
 package br.com.senacrs.alp.aulas;
 
+// Padrão de projeto <Singleton>
 
 public class Somatorio {
-	
+
 	private static Somatorio instancia = new Somatorio();
-	
-	
+
 	private Somatorio() {
 
 		super();
 	}
-	
+
 	public double somaTotal(double[] valores) {
-		
+
 		double resultado = 0.0;
-		
-		//Implementar
-		
+
+		// Implementar
+
+		if (valores == null) {
+			resultado = Double.NaN;
+		} else {
+			for (int i = 0; i < valores.length; i++) {
+				resultado = resultado + valores[i];
+			}
+		}
+
 		return resultado;
 	}
-	
+
 	public static Somatorio getInstancia() {
-		
+
 		return Somatorio.instancia;
 	}
 }
